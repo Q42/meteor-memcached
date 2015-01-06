4f1d5922e292f409ac68e06be957acb8d84f202d
Kars Veling <kars@q42.nl>
2015-01-06 23:00:32 +0100

    package.js and lib folder cleanup

--- package.js
+++ package.js
@@ -1,24 +1,17 @@
 Package.describe({
   name: 'q42:memcached',
-  summary: 'Memcached client for Meteor. Wrapped 3rd-eden memcached module, based on version 2.0.0: https://github.com/3rd-Eden/node-memcached',
-  version: '2.0.0',
+  summary: 'Memcached client for Meteor. Based on 3rd-eden node-memcached,version 2.0.0',
+  version: '2.0.3',
   git: ' /* Fill me in! */ '
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.0.2');
-  //api.addFiles('q42:memcached.js');
-  api.addFiles('server/utils.js');
-  api.addFiles('server/ms.js');
-  api.addFiles('server/connection-parse.js');
-  api.addFiles('server/simple-lru.js');
-  api.addFiles('server/retry-operation.js');
-  api.addFiles('server/retry.js');
-  api.addFiles('server/jackpot.js');
-  api.addFiles('server/hashring.js');
-  api.addFiles('server/connection.js');
-  api.addFiles('server/memcached.js');
+
+  api.addFiles(['lib/utils.js', 'lib/ms.js', 'lib/connection-parse.js', 'lib/simple-lru.js', 'lib/retry-operation.js',
+  'lib/retry.js','lib/jackpot.js','lib/hashring.js','lib/connection.js','q42:memcached.js'], 'server');
   api.export('Memcached');
+
 });
 
 Package.onTest(function(api) {
